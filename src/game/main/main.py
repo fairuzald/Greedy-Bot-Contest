665bedd540fae3ca701a8908d4a0c3518e258547
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-03-02 21:32:01 +0700

    feat: update existing

--- src/game/main/main.py
+++ src/game/main/main.py
@@ -1,50 +1,40 @@
 import random
-from typing import Optional, List
+from typing import Optional, List, Tuple
 from game.logic.base import BaseLogic
+from game.service.teleport_services import TeleportService
 from game.models import GameObject, Board, Position
-from ..util import get_direction
+from ..util import get_direction, position_equals
 
 class AlucardGreedy(BaseLogic):
     def __init__(self):
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         self.goal_position: Optional[Position] = None
         self.current_direction = 0
-    def teleport_positions(self, teleport: List[GameObject]):
-        position_array = [d.position for d in teleport]
-        return position_array
-
-    def get_distance_teleport_bot(self, bot: GameObject, teleport: List[GameObject]):
-        array_dis = [(dis.x - bot.position.x, dis.y - bot.position.y) for dis in self.teleport_positions(teleport)]
-        return array_dis
-
-    def get_nearest_teleport(self, bot: GameObject, teleport: List[GameObject]):
-        array_dis = self.get_distance_teleport_bot(bot, teleport)
-        min_distance = float('inf')
-        nearest_position = None
-
-        for d in array_dis:
-            x, y = d
-            distance = abs(x - bot.position.x) + abs(y - bot.position.y)
-
-            if distance < min_distance:
-                min_distance = distance
-                nearest_position = Position(x, y)
-
-        return nearest_position
+        self.logic = False
+    
     def next_move(self, board_bot: GameObject, board: Board):
         props = board_bot.properties
+        bot_position = board_bot.position
         teleport = board.teleport
-        nearest_teleport = self.get_nearest_teleport(board_bot, teleport)
+        diamonds = board.diamonds
+        service = TeleportService()
+        nearest_teleport = service.get_nearest_teleport(bot_position, teleport)
+        tel_nearest =service.get_nearest_diamond_by_teleport(bot_position, diamonds, teleport)
+        if self.logic == False:
+            self.logic = True
+            self.goal_position = tel_nearest
 
-        print(nearest_teleport)
+    
+        print(f"Nearest teleport: {tel_nearest}")
         # Analyze new state
-        if props.diamonds == 5:
-            # Move to base
-            base = board_bot.properties.base
-            self.goal_position = base
-        else:
-            # Just roam around
-            self.goal_position = None
+        
+        # if props.diamonds == 5:
+        #     # Move to base
+        #     base = board_bot.properties.base
+        #     self.goal_position = base
+        # else:
+        #     # Just roam around
+        #     self.goal_position = None
 
         current_position = board_bot.position
         if self.goal_position:
@@ -56,6 +46,9 @@ class AlucardGreedy(BaseLogic):
                 self.goal_position.y,
             )
             print(f"Moving to {self.goal_position} delta {delta_x} {delta_y}")
+            if(position_equals(current_position, self.goal_position) ):
+                self.goal_position = None
+                self.logic = False
         else:
             # Roam around
             delta = self.directions[self.current_direction]
--- src/game/service/teleport_services.py
+++ src/game/service/teleport_services.py
@@ -1,6 +1,50 @@
-# @dataclass
-# class TeleportService:
 
-#     def getTelportDiamond(self, x: int, y: int):
-#         self.player.x = x
-#         self.player.y = y
+from typing import List
+from game.models import Position, GameObject, Board
+
+class TeleportService:
+    def __init__(self):
+        pass
+    
+    # Get the distance between bot and 1 teleport
+    def get_distance(self, origin: Position, target: Position) -> int:
+        return abs(target.x - origin.x) + abs(target.y - origin.y) 
+
+    # Get the nearest teleport from the bot
+    def get_nearest_teleport(self, curr:Position, teleports: List[GameObject]) -> Position:
+        min_distance = float('inf')
+        nearest_position = None
+
+        for tel in teleports:
+            # Get distance data
+            distance = self.get_distance(curr, tel.position)
+            
+            # Minimum Comparation
+            if distance < min_distance:
+                min_distance = distance
+                nearest_position = tel.position
+                
+        return nearest_position
+    
+    # GEt the distance between bot and diamond by using teleport
+    def get_distance_diamond_bot_by_teleport(self, curr: Position, diamond: Position, teleport: Position) -> int:
+        return self.get_distance(curr, teleport) + self.get_distance(teleport, diamond)
+    
+    # Get the nearest diamond from the bot using teleport
+    def get_nearest_diamond_by_teleport(self, curr: Position, diamonds: List[GameObject], teleports: List[GameObject]) -> Position:
+        min_distance = float('inf')
+        nearest_teleport = None
+
+        for diamond in diamonds:
+            for tel in teleports:
+                # Get distance data
+                distance = self.get_distance_diamond_bot_by_teleport(curr, diamond.position, tel.position)
+                
+                # Minimum Comparation
+                if distance < min_distance:
+                    min_distance = distance
+                    nearest_teleport = tel.position
+                
+        return nearest_teleport
+    
+    
