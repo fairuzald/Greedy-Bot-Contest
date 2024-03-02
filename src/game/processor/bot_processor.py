fa16606fbac3983455f75a398d3dc9118060060d
Yusufarsan <sandiyusufardian@gmail.com>
2024-03-02 20:39:13 +0700

    feat: added process to change detection enemy status

--- src/game/processor/bot_processor.py
+++ src/game/processor/bot_processor.py
@@ -1,5 +1,5 @@
 from processor.processor import Processor
-from game.models import GameObject, Board
+from game.models import GameObject, Board, Position
 
 class BotProcessor(Processor):
     # Nyala kalau nyampe threshold
@@ -10,6 +10,24 @@ class BotProcessor(Processor):
 
     def __init__(self, bot: GameObject, board: Board):
         super().__init__(bot, board)
+        self.arr_position = [       # Semua kemungkinan posisi yang berada pada threshold
+            Position(x=0, y=2),
+            Position(x=1, y=1),
+            Position(x=2, y=0),
+            Position(x=1, y=-1),
+            Position(x=0, y=-2),
+            Position(x=-1, y=-1),
+            Position(x=-2, y=0),
+            Position(x=-1, y=1)
+        ]
 
     def process(self):
         print('FoodProcessor process')
+        # Check surroundings if there are any enemies within threshold
+        # checked position (x,y): (0,2), (1,1), (2,0), (1,-1), (0,-2), (-1,-1), (-2,0), (-1,1)
+        for pos in self.arr_position:
+            if pos == self.bot.position:
+                self.status_bot_processor = True
+                break
+            else:
+                self.status_bot_processor = False
