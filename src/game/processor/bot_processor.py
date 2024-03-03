2eba97b1adfc89966c2224c7f296ffac5d307c79
Yusufarsan <sandiyusufardian@gmail.com>
2024-03-03 12:41:13 +0700

    fix: fix bug

--- src/game/processor/bot_processor.py
+++ src/game/processor/bot_processor.py
@@ -11,23 +11,23 @@ class BotProcessor(Processor):
     def __init__(self, bot: GameObject, board: Board):
         super().__init__(bot, board)
         self.arr_position = [       # Semua kemungkinan posisi yang berada pada threshold
-            Position(x=0, y=2),
-            Position(x=1, y=1),
-            Position(x=2, y=0),
-            Position(x=1, y=-1),
-            Position(x=0, y=-2),
-            Position(x=-1, y=-1),
-            Position(x=-2, y=0),
-            Position(x=-1, y=1)
+            Position(x=0 + bot.position.x, y=2 + bot.position.y),
+            Position(x=1 + bot.position.x , y=1 + bot.position.y),
+            Position(x=2 + bot.position.x, y=0 + bot.position.y),
+            Position(x=1 + bot.position.x, y=-1 + bot.position.y),
+            Position(x=0 + bot.position.x, y=-2 + bot.position.y),
+            Position(x=-1 + bot.position.x, y=-1 + bot.position.y),
+            Position(x=-2 + bot.position.x, y=0 + bot.position.y),
+            Position(x=-1 + bot.position.x, y=1 + bot.position.y)
         ]
 
-    def process(self):
-        print('FoodProcessor process')
+    def bot_process(self):
         # Check surroundings if there are any enemies within threshold
-        # checked position (x,y): (0,2), (1,1), (2,0), (1,-1), (0,-2), (-1,-1), (-2,0), (-1,1)
+        # checked position of bot (x,y): (0,2), (1,1), (2,0), (1,-1), (0,-2), (-1,-1), (-2,0), (-1,1)
         for pos in self.arr_position:
-            if pos == self.bot.position:
-                self.status_bot_processor = True
-                break
-            else:
-                self.status_bot_processor = False
+            for bot in self.board.bots:
+                if pos == bot.position:
+                    self.status_bot_processor = True
+                    break
+                else:
+                    self.status_bot_processor = False
