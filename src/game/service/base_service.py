d1a9f184f037eae4905e7e4600b032a1bfaff56d
Yusufarsan <sandiyusufardian@gmail.com>
2024-03-03 12:28:09 +0700

    feat: added function to check if the goal direction same as base

---
+++ src/game/service/base_service.py
@@ -0,0 +1,31 @@
+from game.models import GameObject, Board, Position
+from ..util import get_direction
+
+class BaseService:
+    def __init__(self, bot: GameObject, board: Board):
+        super().__init__()
+        self.board = board
+        self.bot = bot
+
+    def get_nearest_diamond(self) -> Position:  
+        return (1,0)
+
+    def get_distance(self, curr: Position, tar: Position) -> int:
+        return abs(curr.x - tar.x) + abs(curr.y - tar.y)
+    
+    def get_delta(self, curr_pos: Position, dest_pos: Position) -> tuple[int, int]:
+        delta_x = dest_pos.x - curr_pos.x
+        delta_y = dest_pos.y - curr_pos.y
+        return (delta_x, delta_y)
+
+    def is_base_same_direction(self, curr_pos: Position, goal_pos: Position) -> tuple[bool, Position]:      # Position -> self.goal_position yang baru (yaitu Position base jika bool=True, dan none jika bool=False)
+        delta_x_goal, delta_y_goal = self.get_delta(curr_pos, goal_pos)
+        delta_x_base, delta_y_base = self.get_delta(curr_pos, self.bot.properties.base)
+
+        # Check if kuadran sama
+        if delta_x_goal * delta_x_base >= 0 and delta_y_goal * delta_y_base >= 0:
+            # Checks if the base is in the same direction as the goal
+            if abs(delta_x_goal) >= abs(delta_x_base) and abs(delta_y_goal) >= abs(delta_y_base):
+                return (True, self.bot.properties.base)     # Menghasilkan benar dan posisi base sebagai goal position yang baru
+            
+        return (False, None)    # Menghasilkan false dan none
