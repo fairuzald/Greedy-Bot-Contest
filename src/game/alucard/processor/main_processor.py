5b1fc25012e66a01a7730037b6bf2e71982eddff
Moh. Fairuz Alauddin Yahya <fairuzy210@gmail.com>
2024-03-04 11:39:19 +0700

    feat: predefined all class in main processor

--- src/game/alucard/processor/main_processor.py
+++ src/game/alucard/processor/main_processor.py
@@ -8,6 +8,7 @@ from game.alucard.service.math_services import MathService
 from game.alucard.service.object_services import ObjectServices
 from game.alucard.processor.red_processor import RedProcessor
 from game.util import position_equals
+
 class MainProcessor(Processor):
     # Current proses berisi string berupa teleport/diamond
     curr_process = None
@@ -19,33 +20,34 @@ class MainProcessor(Processor):
 
     def __init__(self, bot: GameObject, board: Board):
         super().__init__(bot, board)
+        self.diamondProcessor = DiamondProcessor(self.bot, self.board)
+        self.teleportProcessor = TeleportProcessor(self.bot, self.board)
+        self.redProcessor = RedProcessor(self.bot, self.board)
 
     def process(self):
-        diamondProcessor = DiamondProcessor(self.bot, self.board)
-        teleportProcessor = TeleportProcessor(self.bot, self.board)
-        redProcessor = RedProcessor(self.bot, self.board)
-        if(self.teleports_position == []):
+        if not self.teleports_position:
             self.teleports_position = [obj.position for obj in ObjectServices.teleport(self.board.game_objects)]
-        if(self.red_position ==[]):
+        if not self.red_position:
             self.red_position = [obj.position for obj in ObjectServices.red_button(self.board.game_objects)]
-        # if teleport in area of bot, then process teleport
-        if(MathService.isObjectInArea(self.bot.position,self.teleports_position,self.teleport_threshold)):
+        
+        # if teleport in the area of bot, then process teleport
+        if MathService.isObjectInArea(self.bot.position, self.teleports_position, self.teleport_threshold):
             self.curr_process = "teleport"
-        elif(self.bot.properties.milliseconds_left <= 7000 and MathService.isObjectInArea(self.bot.position,self.red_position,self.red_threshold) and redProcessor.minimum):
+        elif self.bot.properties.milliseconds_left <= 7000 and MathService.isObjectInArea(self.bot.position, self.red_position, self.red_threshold) and self.redProcessor.minimum:
             self.curr_process = "red"
         else:
             self.curr_process = "diamond"
             
-        if(self.curr_process == "teleport"):
-            teleportProcessor.process()
-            self.goal_position = teleportProcessor.goal_position
-            self.curr_process = teleportProcessor.curr_process
-        elif(self.curr_process == "red"):
+        if self.curr_process == "teleport":
+            self.teleportProcessor.process()
+            self.goal_position = self.teleportProcessor.goal_position
+            self.curr_process = self.teleportProcessor.curr_process
+        elif self.curr_process == "red":
             self.goal_position = self.red_position[0]
-            if(position_equals(self.bot.position, self.red_position[0])):
+            if position_equals(self.bot.position, self.red_position[0]):
                 self.curr_process = "diamond"
         else:
-            diamondProcessor.process()
-            self.goal_position = diamondProcessor.goal_position
+            self.diamondProcessor.process()
+            self.goal_position = self.diamondProcessor.goal_position
+
         print("Current Process: ", self.curr_process)
-        
